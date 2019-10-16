10dca4f17daa6f847c70d93642a66e07835a8026
AleDuranR <aduranrocha@gmail.com>
2019-10-15 23:29:34 -0500

    Generation of fibonacci class

---
+++ src/com/firstexercise/app/Excercise25FibonacciSeriesOnlyVariables.java
@@ -0,0 +1,32 @@
+package com.firstexercise.app;
+
+import java.util.Scanner;
+
+public class Excercise25FibonacciSeriesOnlyVariables {
+	public static void main(String[] args) {
+		// Constant
+		
+		// Variables
+		int userNum = 0;
+		int fibonacci = 1;
+		int count1 = 0;
+		int count2 = 1;
+		
+		// Object declaration
+		Scanner scan = new Scanner(System.in);
+		
+		// Input
+		userNum = scan.nextInt();
+		
+		// Process
+		// Do the process the number of times the user asked
+		for(int i= 0; i < userNum; i++) {
+			// Print fibonacci number
+			System.out.print(" " + fibonacci);
+			
+			fibonacci = count1+count2;
+			count1 = count2;
+			count2 = fibonacci;
+		}
+	}
+}
